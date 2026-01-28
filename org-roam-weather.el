da3ac6d98cf22c62570ec7863ffa51e490b8870d
Edward Doolittle <edward.doolittle@gmail.com>
2026-01-28 00:46:41 -0600

    .el to place current weather in org-roam dailies

---
+++ org-roam-weather.el
@@ -0,0 +1,58 @@
+;; a little shell command to get the weather in a location
+;; substitute Regina with correct string for your locality
+
+(defun org-roam-weather (current-node-ignored)
+  "Return local weather as a string for Regina for org-roam capture."
+  (shell-command-to-string "curl 'https://wttr.in/Regina?0T' 2>/dev/null"))
+
+;; Here's my current org-roam setup.  Every daily entry starts with the
+;; current weather in graphical format
+
+(use-package org-roam
+  :ensure t
+  :init
+  (setq org-roam-v2-ack t)
+  :custom
+  (org-roam-directory "~/org-roam")
+  (org-roam-completion-everywhere t)
+  (org-roam-capture-templates
+   '(("d" "default" plain
+      "%?"
+      :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n#+date: %U\n")
+      :unnarrowed t)
+     ("l" "programming language" plain
+      "* Characteristics\n\n- Family: %?\n- Inspired by: \n\n* Reference:\n\n"
+      :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n#+date: %U\n")
+      :unnarrowed t)
+     ("b" "book notes" plain
+      (file "~/org-roam/templates/bookNoteTemplate.org")
+      :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n#+date: %U\n")
+      :unnarrowed t)
+     ("p" "project" plain "* Goals\n\n%?\n\n* Tasks\n\n** TODO Add initial tasks\n\n* Dates\n\n"
+      :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n#+date: %U\n#+filetags: Project")
+      :unnarrowed t)
+     ("w" "weather" plain
+      "${org-roam-weather}\n%?"
+      :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n#+date: %U\n")
+      :unnarrowed t)
+     )
+   )
+;; moved Topic before date/time in case want TODO
+  (org-roam-dailies-capture-templates
+   '(("d" "default" entry "* %^{Topic} %<%Y-%m-%d> %<%I:%M %p>\n${org-roam-weather}\n%?"
+      :if-new (file+head "%<%Y-%m-%d>.org" "#+title: %<%Y-%m-%d>\n"))))
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+	 ("C-c n f" . org-roam-node-find)
+	 ("C-c n i" . org-roam-node-insert)
+	 :map org-mode-map
+	 ("C-M-i" . completion-at-point)
+	 :map org-roam-dailies-map
+	 ("Y" . org-roam-dailies-capture-yesterday)
+	 ("T" . org-roam-dailies-capture-tomorrow)
+	 )
+  :bind-keymap
+  ("C-c n d" . org-roam-dailies-map)
+  :config
+  (require 'org-roam-dailies) ;; Ensure the keymap is available
+  (org-roam-db-autosync-mode))
+

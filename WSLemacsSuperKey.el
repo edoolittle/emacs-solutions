15b970ca5a7515629416b5953b7c756c9f3d4c74
Edward Doolittle <edward.doolittle@gmail.com>
2026-01-28 00:38:10 -0600

    added support for super-left and super-right

--- WSLemacsSuperKey.el
+++ WSLemacsSuperKey.el
@@ -5,7 +5,7 @@
 (define-key function-key-map (kbd "<f9>") 'event-apply-super-modifier)
 
 ;; example application of super key to simulate LWin downarrow which
-;; in Windows has the effect of minimizing a window
+;; in Windows has the effect of minimizing a window; requires xdotool
 
 (global-set-key (kbd "s-<down>") 
   (lambda ()
@@ -15,3 +15,11 @@
   (lambda ()
     (interactive)
     (shell-command-to-string "xdotool getactivewindow windowsize 100% 100% && xdotool getactivewindow windowmove 0 0")))
+(global-set-key (kbd "s-<left>") 
+  (lambda ()
+    (interactive)
+    (shell-command-to-string "xdotool getactivewindow windowsize 50% 100% && xdotool getactivewindow windowmove 0 0")))
+(global-set-key (kbd "s-<right>") 
+  (lambda ()
+    (interactive)
+    (shell-command-to-string "xdotool getactivewindow windowsize 50% 100% && xdotool getactivewindow windowmove 50% 0")))

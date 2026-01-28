b433e1dc17c0200e539abbcc7a13f663d2b87d81
Edward Doolittle <edward.doolittle@gmail.com>
2026-01-27 23:43:54 -0600

    added .el with support for WSLemacsSuperKey.ahk

---
+++ WSLemacsSuperKey.el
@@ -0,0 +1,13 @@
+;; emacs support to complement WSLemacSuperKey.ahk
+;; add this fragment to init.el
+
+(setq frame-title-format "%b (GNU Emacs)")
+(define-key function-key-map (kbd "<f9>") 'event-apply-super-modifier)
+
+;; example application of super key to simulate LWin downarrow which
+;; in Windows has the effect of minimizing a window
+
+(global-set-key (kbd "s-<down>") 
+  (lambda ()
+    (interactive)
+    (shell-command-to-string "xdotool getactivewindow windowminimize")))

5a6ac6ced3a2c1baae2838612fd50c90e8a55a00
Edward Doolittle <edward.doolittle@gmail.com>
2026-02-07 16:35:10 -0600

    fixed emacsclient programs

---
+++ emacsclient_MAC.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# To get the most of this script, add the following lines to init.el
+#
+#  (defun split-window-2-files (f1 f2) 
+#    (find-file f1)
+#    (find-file-other-window f2))
+#
+# (without the shell comment symbols # of course) and check that
+# your emacs app is in the list in the if statement immediately
+# below
+#
+# Also, install nircmd.exe in your path
+
+
+MY_EMACS="/Applications/MacPorts/EmacsMac.app/Contents/MacOS/Emacs"
+MACOS_EMACS=yes
+
+
+# Undocumented behaviour: emacsclient -a /bin/false returns false
+# if there is no emacs running, otherwise returns true
+# See https://www.emacswiki.org/emacs/EmacsPipe
+
+if ! emacsclient -a /bin/false -e '()' > /dev/null 2>&1; then
+    if [[ "$1" == "-" ]]; then
+        TMP="$(mktemp /tmp/emacsstdinXXX)";
+        cat >"$TMP";
+       	$MY_EMACS --eval "(let ((b (create-file-buffer \"*stdin*\"))) (switch-to-buffer b) (insert-file-contents \"${TMP}\") (delete-file \"${TMP}\"))" &
+    elif [ $# -eq 2 ]; then
+	    $MY_EMACS -e "(split-window-2-files \"$1\" \"$2\")" > /dev/null 2>&1 
+    else
+	    $MY_EMACS "$@" &
+    fi
+	sleep 2
+	osascript -e 'tell application "System Events" to click UI element "EmacsMac" of list 1 of application process "Dock"' > /dev/null 2>&1
+else
+    if [[ "$1" == "-" ]]; then
+        TMP="$(mktemp /tmp/emacsstdinXXX)";
+        cat >"$TMP";
+        emacsclient -u -c -n -e "(let ((b (create-file-buffer \"*stdin*\"))) (switch-to-buffer b) (insert-file-contents \"${TMP}\") (delete-file \"${TMP}\"))"
+    elif [ $# -eq 2 ]; then
+ 	    emacsclient -u -c -n -e "(split-window-2-files \"$1\" \"$2\")"
+    else
+	    emacsclient -u -c -n "$@"
+    fi
+	osascript -e 'tell application "System Events" to click UI element "EmacsMac" of list 1 of application process "Dock"' > /dev/null 2>&1
+fi
+
---
+++ emacsclient_WSL.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# To get the most of this script, add the following lines to init.el
+#
+#  (defun split-window-2-files (f1 f2) 
+#    (find-file f1)
+#    (find-file-other-window f2))
+#
+# (without the shell comment symbols # of course) and check that
+# your emacs app is in the list in the if statement immediately
+# below
+#
+# Also, install nircmd.exe in your path
+
+
+#MY_EMACS='/usr/bin/emacs-w32'
+MY_EMACS='emacs'
+
+# Undocumented behaviour: emacsclient -a /bin/false returns false
+# if there is no emacs running, otherwise returns true
+# See https://www.emacswiki.org/emacs/EmacsPipe
+
+if ! emacsclient -a /bin/false -e '()' > /dev/null 2>&1; then
+    if [[ "$1" == "-" ]]; then
+        TMP="$(mktemp /tmp/emacsstdinXXX)";
+        cat >"$TMP";
+       	$MY_EMACS --eval "(let ((b (create-file-buffer \"*stdin*\"))) (switch-to-buffer b) (insert-file-contents \"${TMP}\") (delete-file \"${TMP}\"))" &
+        nircmd.exe win activate stitle "\*stdin\*"
+    elif [ $# -eq 2 ]; then
+	    $MY_EMACS -e "(split-window-2-files \"$1\" \"$2\")" > /dev/null 2>&1 
+        nircmd.exe win activate stitle "${2}(GNU Emacs)"
+    else
+	    $MY_EMACS "$@" &
+        nircmd.exe win activate stitle "${1}(GNU Emacs)"
+    fi
+else
+    if [[ "$1" == "-" ]]; then
+        TMP="$(mktemp /tmp/emacsstdinXXX)";
+        cat >"$TMP";
+        emacsclient -u -c -n -e "(let ((b (create-file-buffer \"*stdin*\"))) (switch-to-buffer b) (insert-file-contents \"${TMP}\") (delete-file \"${TMP}\"))"
+        nircmd.exe win activate stitle "\*stdin\*"
+    elif [ $# -eq 2 ]; then
+ 	    emacsclient -u -c -n -e "(split-window-2-files \"$1\" \"$2\")"
+        nircmd.exe win activate stitle "${2}(GNU Emacs)"
+    else
+	    emacsclient -u -c -n "$@"
+        nircmd.exe win activate stitle "${1}(GNU Emacs)"
+    fi
+fi
+
